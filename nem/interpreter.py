b40d59d5228654e860a7b96313a386d69407dd83
Nemanja Mirić <nemanjamiric@outlook.com>
2020-04-21 19:05:50 +0200

    Added all tokens and improved lexer tests.

--- nem/interpreter.py
+++ nem/interpreter.py
@@ -24,7 +24,7 @@ class Interpreter:
         Initializes the Interpreter class and adds a newline character to the end of the Nem code for handling EOF.
 
         """
-        self.code = "{}\n".format(code.lower())
+        self.code = "{}\n".format(code)
 
     def lex(self):
         """Lex the code.
@@ -33,13 +33,35 @@ class Interpreter:
 
         List of tokens:
         ',' - <list-element>
-        '"..."' - <text>
-        'input' - <stream>; 'output' - <stream>; 'error' - <stream>
-        r'[=+-*/^%]' - <operator>
-        r'[0-9]*[.]?[0-9]+' - <number>
-        r'[a-z_][a-z_0-9]*' - <symbol>
+        ';' - <end>
+        r'[=+-*/^%<>]' - <operator>
         '(' - <left-bracket>
         ')' - <right-bracket>
+        '[' - <left-square>
+        ']' - <right-square>
+        '"..."' - <text>
+        r'[0-9]*[.]?[0-9]+' - <number>
+        r'[a-z_][a-z_0-9]*' - <symbol>
+        'null' - <null>
+        'input' - <keyword>
+        'output' - <keyword>
+        'error' - <keyword>
+        'while' - <keyword>
+        'if' - <keyword>
+        'otherwise' - <keyword>
+        'import' - <keyword>
+        'open' - <keyword>
+        'convert' - <keyword>
+        'number' - <keyword>
+        'text' - <keyword>
+        'list' - <keyword>
+        'break' - <keyword>
+        'continue' - <keyword>
+        'function' - <keyword>
+        'not' - <operator>
+        'is' - <operator>
+        'and' - <operator>
+        'or' - <operator>
 
         Gets ignored:
         '#...\n' - Comment
@@ -61,19 +83,29 @@ class Interpreter:
             elif current_token == ",":
                 tokens.append(Token("list-element", current_token))
 
+            elif current_token == ";":
+                tokens.append(Token("end", current_token))
+
+            elif current_token in "=+-*/^%<>":
+                tokens.append(Token("operator", current_token))
+
+            elif current_token == "[":
+                tokens.append(Token("left-square", current_token))
+
+            elif current_token == "]":
+                tokens.append(Token("right-square", current_token))
+
             elif current_token == "(":
                 tokens.append(Token("left-bracket", current_token))
 
             elif current_token == ")":
                 tokens.append(Token("right-bracket", current_token))
 
-            elif current_token in "=+-*/^%":
-                tokens.append(Token("operator", current_token))
-
             elif current_token == "#":
                 while self.code[current_index] != "\n":
                     current_index += 1
-                    character += 1
+                line += 1
+                character = 0
 
             elif current_token == "\"":
                 current_index += 1
@@ -88,15 +120,22 @@ class Interpreter:
                 else:
                     tokens.append(Token("text", self.code[start_index:current_index]))
 
-            elif re.match("[a-z_]", current_token):
+            elif re.match("[a-zA-Z_]", current_token):
                 start_index = current_index
                 current_index += 1
                 character += 1
-                while re.match("[a-z_][a-z_0-9]*\Z", self.code[start_index:current_index + 1]):
+                while re.match("[a-zA-Z_][a-zA-Z_0-9]*\Z", self.code[start_index:current_index + 1]):
                     current_index += 1
                     character += 1
-                if self.code[start_index:current_index] in ("input", "output", "error"):
-                    tokens.append(Token("stream", self.code[start_index:current_index]))
+                if self.code[start_index:current_index] in ("input", "output", "error", "while", "if",
+                                                            "otherwise", "import", "open", "convert", "number",
+                                                            "text", "list", "break", "continue", "function"
+                                                            ):
+                    tokens.append(Token("keyword", self.code[start_index:current_index]))
+                elif self.code[start_index:current_index] == "null":
+                    tokens.append(Token("null", self.code[start_index:current_index]))
+                elif self.code[start_index:current_index] in ("not", "is", "and", "or"):
+                    tokens.append(Token("operator", self.code[start_index:current_index]))
                 else:
                     tokens.append(Token("symbol", self.code[start_index:current_index]))
                 continue
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -25,76 +25,15 @@ class InterpreterTestCase(unittest.TestCase):
         Tests the interpreter's lexer.
 
         """
-        code = """
-            # Code made for testing purposes #
-            my_very_cool_variable123 = 77
-            _0=.0
-            a   =  "a string123;'-p="
-            
-            # Do some stream stuff
-            output a
-            error my_very_cool_variable123
-            input a
-            output a
-            output "some text1 2   3"
-            
-            # Math stuff
-            2 + 2 * 2
-            555 + .2 * 1^3
-            66^77
-            gg = 1-2+3-4+5-6
-            d = 5x^2 + x^1 %3x^0#Cool stuff
-            output d
-            
-            (2+2)*2^2
-            
-            list = 1, 2,3 ,4 ,6 ,7 #neat"""
+        self.maxDiff = None
 
-        self.assertEqual(Interpreter(code).lex(),
-                         [Token('symbol', 'my_very_cool_variable123'), Token('operator', '='),
-                          Token('number', '77'), Token('symbol', '_0'),
-                          Token('operator', '='), Token('number', '.0'),
-                          Token('symbol', 'a'), Token('operator', '='),
-                          Token('text', "a string123;'-p="), Token('stream', 'output'),
-                          Token('symbol', 'a'), Token('stream', 'error'),
-                          Token('symbol', 'my_very_cool_variable123'),
-                          Token('stream', 'input'), Token('symbol', 'a'),
-                          Token('stream', 'output'), Token('symbol', 'a'),
-                          Token('stream', 'output'), Token('text', 'some text1 2   3'),
-                          Token('number', '2'), Token('operator', '+'),
-                          Token('number', '2'), Token('operator', '*'),
-                          Token('number', '2'), Token('number', '555'),
-                          Token('operator', '+'), Token('number', '.2'),
-                          Token('operator', '*'), Token('number', '1'),
-                          Token('operator', '^'), Token('number', '3'),
-                          Token('number', '66'), Token('operator', '^'),
-                          Token('number', '77'), Token('symbol', 'gg'),
-                          Token('operator', '='), Token('number', '1'),
-                          Token('operator', '-'), Token('number', '2'),
-                          Token('operator', '+'), Token('number', '3'),
-                          Token('operator', '-'), Token('number', '4'),
-                          Token('operator', '+'), Token('number', '5'),
-                          Token('operator', '-'), Token('number', '6'),
-                          Token('symbol', 'd'), Token('operator', '='),
-                          Token('number', '5'), Token('symbol', 'x'),
-                          Token('operator', '^'), Token('number', '2'),
-                          Token('operator', '+'), Token('symbol', 'x'),
-                          Token('operator', '^'), Token('number', '1'),
-                          Token('operator', '%'), Token('number', '3'),
-                          Token('symbol', 'x'), Token('operator', '^'),
-                          Token('number', '0'), Token('stream', 'output'),
-                          Token('symbol', 'd'), Token('left-bracket', '('),
-                          Token('number', '2'), Token('operator', '+'),
-                          Token('number', '2'), Token('right-bracket', ')'),
-                          Token('operator', '*'), Token('number', '2'),
-                          Token('operator', '^'), Token('number', '2'),
-                          Token('symbol', 'list'), Token('operator', '='),
-                          Token('number', '1'), Token('list-element', ','),
-                          Token('number', '2'), Token('list-element', ','),
-                          Token('number', '3'), Token('list-element', ','),
-                          Token('number', '4'), Token('list-element', ','),
-                          Token('number', '6'), Token('list-element', ','),
-                          Token('number', '7')])
+        with open("test_interpreter_lex.in") as _input:
+            _input = _input.read()
+
+        with open("test_interpreter_lex.out") as output:
+            output = eval(output.read())
+
+        self.assertEqual(Interpreter(_input).lex(), output)
 
         with self.assertRaises(LexerException):
             Interpreter("\n\"\"\"").lex()
